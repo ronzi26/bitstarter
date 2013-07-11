e8e0200ea15b970d6e1a759d3c63e95821d41e2b
Roni Toledano <roni@bilbeo.com>
2013-07-11 09:47:28 +0000

    revert

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var text = fs.readFileSync('index.html', 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(text); 
+  response.send(fs.readFileSync('index.html', 'utf-8')); 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
