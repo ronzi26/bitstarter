23cb873b8fd96145a5c7bd51d3817ecf54a4993c
Roni Toledano <roni@bilbeo.com>
2013-07-11 10:38:57 +0000

    buffer

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var FILE_DEFAULT = "index.html";
+file = file || FILE_DEFAULT;
+var buffer = new Buffer(fs.readFileSync(file));
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf-8')); 
+  response.send(buffer.toString()); 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
