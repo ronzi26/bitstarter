3b0ed8be5d8211495395acdaa3735c062890cb8c
Roni Toledano <roni@bilbeo.com>
2013-07-24 19:26:12 +0000

    Add Bootstrap and included responsive css

--- web.js
+++ web.js
@@ -7,13 +7,15 @@ var FILE_DEFAULT = "index.html";
 
 var buffer = new Buffer(fs.readFileSync(FILE_DEFAULT));
 
+app.use(express.static(__dirname + '/bootstrap'));
+
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
- 
+
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-    
