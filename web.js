f81253840e8c4e6d5981d759f576ec054dceb65c
Roni Toledano <roni@bilbeo.com>
2013-08-19 09:01:49 +0000

    web.js with new express version

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
+app.use(express.logger());
 
 var fs = require('fs');
 var FILE_DEFAULT = "index.html";
@@ -8,12 +9,11 @@ var FILE_DEFAULT = "index.html";
 var buffer = new Buffer(fs.readFileSync(FILE_DEFAULT));
 
 app.use(express.static(__dirname + '/bootstrap'));
-app.use(express.static(__dirname + '/img'));
+
 
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
-
 
 
 var port = process.env.PORT || 8080;
